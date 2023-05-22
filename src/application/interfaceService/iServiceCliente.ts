d5c87b7c489c19347f3613b8919307d6f17d1622
JONATHAN DA SILVA PEREIRA <jhoemen@gmail.com>
2023-05-21 21:46:55 -0400

    Removação pastar e arquivos não utilizados

--- src/application/interfaceService/iServiceCliente.ts
+++
@@ -1,6 +0,0 @@
-import { Cliente } from '../../domain/entities/cliente'
-
-export interface IServiceCliente {
-  findById(id: string): Promise<Cliente | null>
-  create(cliente: Cliente): Promise<void>
-}
--- src/domain/repositories/iClienteRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Cliente } from '../entities/cliente'
-
-export interface IClienteRepository {
-  findById(id: string): Promise<Cliente | null>
-  findByCpf(cpf: string): Promise<Cliente | null>
-  findByEmail(email: string): Promise<Cliente | null>
-  create(cliente: Cliente): Promise<void>
-}
